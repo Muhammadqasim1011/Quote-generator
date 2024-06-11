07e37e32652f4e84fd47012d880de9478dcef659
Muhammad Qasim <111875455+Muhammadqasim1011@users.noreply.github.com>
2024-06-11 12:39:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const apiURL =  "https://api.quotable.io/random";
+
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+async function getquote(url){
+    const response = await fetch(url);
+    var data = await response.json();
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
+}
+
+getquote(apiURL);
+
+function tweet(){
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "---- by " + author.innerHTML, "Tweet Window", "width=800, height=499" );
+}
